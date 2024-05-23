913c536f7a06cf4144f3074b69f029c5e4b5d7f5
wuker <llwaeva@gmail.com>
2024-05-23 16:14:37 -0500

    v02: read file, increase number

--- helo.js
+++ helo.js
@@ -1,1 +1,16 @@
-console.log("I love this game.")
+var fs = require("fs");
+const path = './data.txt'
+
+try {
+    fs.readFileSync(path, 'utf8');
+} catch (err) {
+    fs.writeFileSync(path, '0', { flag: 'a+' }, err => {});
+}
+
+const fileStr = fs.readFileSync(path).toString()
+
+val = parseInt(fileStr, 10) + 1
+
+console.log("The local value is ", val.toString())
+
+fs.writeFileSync(path, val.toString())    
